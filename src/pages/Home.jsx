3d20c18497d27ebdfaf9edd5d31f1c467ad3abf6
SubZzzero <lukyanenko962@gmail.com>
2026-01-02 18:54:35 +0200

    add projects section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "./sections/Hero"
 import TrendingGames from "./sections/TrendingGames";
 import About from "./sections/About";
 import Process from "./sections/Process";
-
+import Projects from "./sections/Projects";
 
 const Home = () => {
     return (<>
@@ -10,6 +10,7 @@ const Home = () => {
         <TrendingGames></TrendingGames>
         <About></About>
         <Process></Process>
+        <Projects></Projects>
     </>
     )
 }
---
+++ src/pages/sections/Projects.jsx
@@ -0,0 +1,60 @@
+import { Box, Typography, Button } from "@mui/material"
+import proj1 from "../../assets/proj1.png"
+import proj2 from "../../assets/proj2.png"
+import proj3 from "../../assets/proj3.png"
+import proj4 from "../../assets/proj4.png"
+import proj5 from "../../assets/proj5.png"
+import proj6 from "../../assets/proj6.png"
+
+const Projects = () => {
+    return <Box
+        sx={{
+            maxWidth: "1300px",
+            width: "100%",
+            padding: "0 15px",
+            margin: "0 auto",
+            position: "relative",
+        }}
+    >
+
+        <Typography sx={{
+            marginTop: "150px",
+            marginBottom: "15px",
+            textAlign: "center",
+            fontSize: "30px",
+            fontWeight: "bold"
+        }}>Our Recent Projects</Typography>
+
+        <Typography sx={{
+            textAlign: "center",
+            width: "700px",
+            margin: "0 auto",
+            fontSize: "14px",
+            marginBottom: "70px"
+
+        }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae necessitatibus neque veritatis, itaque cupiditate maxime corrupti consectetur quo ipsam magni.</Typography>
+
+
+        <Box sx={{
+            display: "flex",
+            alignItems: "center",
+
+            flexWrap: "wrap",
+            gap: "70px",
+            marginBottom: "70px"
+        }}>
+            <img src={proj1} alt="proj1" />
+            <img src={proj2} alt="proj2" />
+            <img src={proj3} alt="proj3" />
+            <img src={proj4} alt="proj4" />
+            <img src={proj5} alt="proj5" />
+            <img src={proj6} alt="proj6" />
+        </Box>
+        <Box sx={{ textAlign: "center" }}><Button variant="contained" size="large" sx={{
+            backgroundColor: "#24201C"
+        }}>SEE ALL</Button></Box>
+    </Box >
+}
+
+
+export default Projects;
